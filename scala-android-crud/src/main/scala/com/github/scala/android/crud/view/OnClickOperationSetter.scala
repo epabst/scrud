739676ab8391a034de44cb5e560785683928f67f
Eric Pabst <epabst@gmail.com>
2012-03-30 06:00:01 -0600

    Specify the UriPath for the OnClickListener.

--- scala-android-crud/src/main/scala/com/github/scala/android/crud/view/OnClickOperationSetter.scala
+++ scala-android-crud/src/main/scala/com/github/scala/android/crud/view/OnClickOperationSetter.scala
@@ -2,20 +2,19 @@ package com.github.scala.android.crud.view
 
 import android.view.View
 import com.github.scala.android.crud.action.{ActivityWithVars, Operation}
-import com.github.scala.android.crud.common.UriPath
-import com.github.triangle.SetterUsingItems
+import com.github.triangle.{SetterUsingItems,&&}
 import com.github.scala.android.crud.view.AndroidConversions._
-import com.github.scala.android.crud.{CrudContextField, CrudContext}
+import com.github.scala.android.crud.{UriField, CrudContextField, CrudContext}
 
 /** A Setter that invokes an Operation when the View is clicked.
   * @author Eric Pabst (epabst@gmail.com)
   */
 case class OnClickOperationSetter[T](viewOperation: View => Operation) extends SetterUsingItems[T]  {
   override def setterUsingItems = {
-    case (view: View, CrudContextField(Some(CrudContext(activity: ActivityWithVars, _)))) => ignoredValue => {
+    case (view: View, CrudContextField(Some(CrudContext(activity: ActivityWithVars, _))) && UriField(Some(uri))) => ignoredValue => {
       if (view.isClickable) {
         view.setOnClickListener { view: View =>
-          viewOperation(view).invoke(UriPath.EMPTY, activity)
+          viewOperation(view).invoke(uri, activity)
         }
       }
     }
