81dc8a8332af9d610435f4387872542eaae276f3
Eric Pabst <epabst@gmail.com>
2012-04-28 07:22:14 -0600

    g8 jberkel/android-app
    Params:
      package: com.github.scrud.android
      name: Scrud Android
      main_activity: (default)
      scala_version: 2.8.1 (since that's what scala-android-crud is at right now to ease the port)
      api_level: (default of 10)
      useProguard: (default of true)

---
+++ src/test/scala/Specs.scala
@@ -0,0 +1,10 @@
+import com.github.scrud.android
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+
+class Specs extends FunSpec with ShouldMatchers {
+  describe("a spec") {
+    it("should do something") {
+    }
+  }
+}
