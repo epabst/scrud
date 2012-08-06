b398742f5b26abbb4d75b161116af761d5764a02
Eric Pabst <epabst@gmail.com>
2012-08-06 15:20:52 -0600

    Extract simple scrud-android/build.sbt.

--- project/build.scala
+++ project/build.scala
@@ -42,18 +42,7 @@ object AndroidBuild extends Build {
   lazy val scrud = Project (
     "scrud-android",
     file("scrud-android"),
-    settings = Defaults.defaultSettings ++ General.settings ++ AndroidBase.settings ++ Seq(
-      libraryDependencies += "com.github.epabst.triangle" % "triangle" % "0.6-SNAPSHOT",
-      libraryDependencies += "com.github.epabst.scrud" % "scrud-android-res" % General.projectVersion artifacts(
-        Artifact("scrud-android-res"), Artifact("scrud-android-res", "apklib", "apklib")),
-      libraryDependencies += "org.slf4j" % "slf4j-jdk14" % "1.6.1" % "test",
-      libraryDependencies += "org.slf4j" % "slf4j-android" % "1.6.1-RC1",
-      libraryDependencies += "org.mockito" % "mockito-core" % "1.8.5" % "test",
-      libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.8.1",
-      libraryDependencies += "junit" % "junit" % "4.8.2" % "test",
-      //todo eliminate easymock as a dependency
-      libraryDependencies += "org.easymock" % "easymock" % "2.5.2" % "test"
-    )
+    settings = Defaults.defaultSettings ++ General.settings ++ AndroidBase.settings
   )
 
   lazy val sample: Project = Project (
