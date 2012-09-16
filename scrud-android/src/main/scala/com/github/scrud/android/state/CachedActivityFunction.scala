69c5f42cad8c47fe0dab5c860e9fb812efb7bfe1
Eric Pabst <epabst@gmail.com>
2012-09-15 23:45:36 -0600

    Add CachedActivityFunction.

---
+++ scrud-android/src/main/scala/com/github/scrud/android/state/CachedActivityFunction.scala
@@ -0,0 +1,37 @@
+package com.github.scrud.android.state
+
+import com.github.scrud.android.{CachedStateListener, CrudContext, ActivityVar}
+import com.github.scrud.android.common.CachedFunction
+import android.os.Bundle
+
+/** A Function whose results are cached in each Activity. */
+trait CachedActivityFunction[A, B] {
+  private val cachedFunctionVar = new ActivityVar[CachedFunction[A, B]]
+
+  /** Specify the actual function to use when the result has not been cached for a given Activity. */
+  protected def evaluate(input: A): B
+
+  def apply(crudContext: CrudContext, input: A): B = {
+    val cachedFunction = cachedFunctionVar.getOrSet(crudContext, {
+      val cachedFunction = CachedFunction[A, B](evaluate)
+      crudContext.addCachedActivityStateListener(new CachedStateListener {
+        /** Save any cached state into the given bundle before switching context. */
+        def onSaveState(outState: Bundle) {}
+
+        /** Restore cached state from the given bundle before switching back context. */
+        def onRestoreState(savedInstanceState: Bundle) {}
+
+        /** Drop cached state.  If stayActive is true, then the state needs to be functional. */
+        def onClearState(stayActive: Boolean) {
+          cachedFunction.clear()
+        }
+      })
+      cachedFunction
+    })
+    cachedFunction(input)
+  }
+
+  def clear(crudContext: CrudContext) {
+    cachedFunctionVar.clear(crudContext)
+  }
+}
