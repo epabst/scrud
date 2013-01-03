e999750c968e7ee03a25cde427f728c98f8e3c29
Eric Pabst <epabst@gmail.com>
2013-01-03 15:32:43 -0700

    Make ListBufferPersistenceFactory store its data in the CrudContext by EntityName.

--- scrud-android/src/main/scala/com/github/scrud/persistence/ListBufferPersistenceFactory.scala
+++ scrud-android/src/main/scala/com/github/scrud/persistence/ListBufferPersistenceFactory.scala
@@ -1,10 +1,10 @@
 package com.github.scrud.persistence
 
-import com.github.scrud.EntityType
+import com.github.scrud.{EntityName, EntityType, CrudContext}
 import collection.mutable
-import com.github.scrud.CrudContext
 import java.util.concurrent.ConcurrentHashMap
 import scala.collection.JavaConversions._
+import com.github.scrud.state.LazyApplicationVal
 
 /**
  * A PersistenceFactory for storing in-memory.
@@ -15,16 +15,18 @@ import scala.collection.JavaConversions._
 class ListBufferPersistenceFactory[T <: AnyRef](instantiateItem: => T) extends PersistenceFactory with DataListenerSetValHolder {
   val canSave = true
 
-  private val persistenceByEntityType: mutable.ConcurrentMap[EntityType,ListBufferCrudPersistence[T]] = new ConcurrentHashMap[EntityType,ListBufferCrudPersistence[T]]()
+  private object PersistenceByEntityName extends LazyApplicationVal[mutable.ConcurrentMap[EntityName,ListBufferCrudPersistence[T]]](
+    new ConcurrentHashMap[EntityName,ListBufferCrudPersistence[T]]())
 
   def newWritable() = instantiateItem
 
   // synchronized so that only one persistence instance is used and onCreateDatabase completes before any thread uses the instance.
   def createEntityPersistence(entityType: EntityType, crudContext: CrudContext) = synchronized {
-    persistenceByEntityType.get(entityType).getOrElse {
+    val persistenceByEntityName = PersistenceByEntityName.get(crudContext)
+    persistenceByEntityName.get(entityType.entityName).getOrElse {
       val persistence = new ListBufferCrudPersistence[T](newWritable(), entityType, crudContext, listenerSet(entityType, crudContext))
+      persistenceByEntityName.putIfAbsent(entityType.entityName, persistence)
       entityType.onCreateDatabase(persistence)
-      persistenceByEntityType.putIfAbsent(entityType, persistence)
       persistence
     }
   }
