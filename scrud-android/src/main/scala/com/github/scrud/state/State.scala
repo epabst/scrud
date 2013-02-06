6fcccecbcc78a5cb953e13a0a573291d47d9bfc9
Eric Pabst <epabst@gmail.com>
2013-02-06 15:36:55 -0700

    Narrow visibility of State.variables.

--- scrud-android/src/main/scala/com/github/scrud/state/State.scala
+++ scrud-android/src/main/scala/com/github/scrud/state/State.scala
@@ -8,7 +8,7 @@ import com.github.scrud.util.SimpleListenerHolder
 /** A container for values of [[com.github.scrud.state.StateVar]]'s */
 trait State extends SimpleListenerHolder[DestroyStateListener] {
   //for some reason, making this lazy results in it being null during testing, even though lazy would be preferrable.
-  private[scrud] val variables: mutable.ConcurrentMap[StateVar[_], Any] = new ConcurrentHashMap[StateVar[_], Any]()
+  private[state] val variables: mutable.ConcurrentMap[StateVar[_], Any] = new ConcurrentHashMap[StateVar[_], Any]()
 
   def onDestroyState() {
     listeners.foreach(_.onDestroyState())
