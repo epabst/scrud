8e6eeee02cbc303cb3048d4733f7263ee7ea9e87
Eric Pabst <epabst@gmail.com>
2013-12-11 15:35:38 -0700

    Add documentation for CrudOperationType.

--- scrud-android/src/main/scala/com/github/scrud/action/CrudOperation.scala
+++ scrud-android/src/main/scala/com/github/scrud/action/CrudOperation.scala
@@ -11,6 +11,12 @@ import com.github.scrud.EntityName
  */
 case class CrudOperation(entityName: EntityName, operationType: CrudOperationType.Value)
 
+/**
+ * The kinds of operations that a user may want to do to one or more entities.
+ * They intentionally mirror the HTTP Methods of POST, GET (on a collection or by id), PUT, and DELETE,
+ * and are intended to be more user-friendly terms.
+ * They explicitly support the concept of interactively creating or updating an entity.
+ */
 object CrudOperationType extends Enumeration {
   val Create = Value("Create")
   val List = Value("List")
