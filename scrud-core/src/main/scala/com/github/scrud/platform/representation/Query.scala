8eb85067c627425789e9abfdfb485c2658cb436d
Eric Pabst <epabst@gmail.com>
2014-02-01 01:30:36 -0700

    Add Query representation.

---
+++ scrud-core/src/main/scala/com/github/scrud/platform/representation/Query.scala
@@ -0,0 +1,11 @@
+package com.github.scrud.platform.representation
+
+import com.github.scrud.copy.StorageType
+
+/**
+ *  A Representation for querying for instances with a matching field value.
+ *  @author Eric Pabst (epabst@gmail.com)
+ *          Date: 12/11/13
+ *          Time: 9:16 AM
+ */
+object Query extends StorageType with Representation
