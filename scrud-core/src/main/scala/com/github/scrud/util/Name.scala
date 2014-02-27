5940d5ec1e9cfbd53b873ef46466b64205a1eaac
Eric Pabst <epabst@gmail.com>
2014-02-26 23:28:29 -0700

    Add Name.toPlural.

--- scrud-core/src/main/scala/com/github/scrud/util/Name.scala
+++ scrud-core/src/main/scala/com/github/scrud/util/Name.scala
@@ -17,8 +17,21 @@ trait Name {
 
   override val toString = name
 
+  /** This will need to be overridden in many cases. */
+  val toPlural: String = name + "s"
+
   /** The name but with the first letter lower-case. */
   lazy val toCamelCase: String = name.charAt(0).toLower + name.substring(1)
 
   lazy val toDisplayableString: String = name.replaceAll("([a-z])([A-Z])", "$1 $2")
+}
+
+object Name {
+  def apply(name: String): Name = {
+    val _name = name
+    new Name {
+      /** A name as a Java identifier such as PoliceOfficer (pascal case) or crescentWrench (camel case). */
+      override def name = _name
+    }
+  }
 }
