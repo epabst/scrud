9410cc0d994137f5af44bfd1e4d26fa6d7211bf9
Eric Pabst <epabst@gmail.com>
2014-03-13 23:39:59 -0600

    Don't assume that EditUI includes SummaryUI or DetailUI.

--- sample-app-core/src/main/scala/com/github/scrud/sample/AuthorEntityType.scala
+++ sample-app-core/src/main/scala/com/github/scrud/sample/AuthorEntityType.scala
@@ -3,14 +3,16 @@ package com.github.scrud.sample
 import com.github.scrud.{EntityName, EntityType}
 import com.github.scrud.platform.PlatformDriver
 import com.github.scrud.types.{NaturalIntQT, TitleQT}
-import com.github.scrud.platform.representation.{DetailUI, SummaryUI, EditUI, Persistence}
+import com.github.scrud.platform.representation._
 import com.github.scrud.copy.types.Validation
 import com.github.scrud.copy.{Derived, Calculation}
+import com.github.scrud.EntityName
+import scala.Some
 
 object Author extends EntityName("Author")
 
 class AuthorEntityType(platformDriver: PlatformDriver) extends EntityType(Author, platformDriver) {
-  val nameField = field("name", TitleQT, Seq(Persistence(1), EditUI, Validation.requiredString))
+  val nameField = field("name", TitleQT, Seq(Persistence(1), EditUI, SelectUI, Validation.requiredString))
 
   val bookCount = field("bookCount", NaturalIntQT, Seq(SummaryUI,
     Calculation { requestContext => Some(requestContext.findAll(Book).size) }))
--- sample-app-core/src/main/scala/com/github/scrud/sample/BookEntityType.scala
+++ sample-app-core/src/main/scala/com/github/scrud/sample/BookEntityType.scala
@@ -3,7 +3,7 @@ package com.github.scrud.sample
 import com.github.scrud.{EntityName, EntityType}
 import com.github.scrud.platform.PlatformDriver
 import com.github.scrud.types.{TitleQT, PositiveIntQT, EnumerationValueQT, DateWithoutTimeQT}
-import com.github.scrud.platform.representation.{SelectUI, EditUI, Persistence}
+import com.github.scrud.platform.representation.{DetailUI, SelectUI, EditUI, Persistence}
 import com.github.scrud.copy.types.{Validation, Default}
 import com.github.scrud.sample.Genre._
 
@@ -12,13 +12,13 @@ object Book extends EntityName("Book")
 class BookEntityType(platformDriver: PlatformDriver) extends EntityType(Book, platformDriver) {
   val nameField = field("name", TitleQT, Seq(Persistence(1), EditUI, SelectUI, Validation.requiredString))
 
-  val author = field(Author, Seq(Persistence(1), EditUI))
+  val author = field(Author, Seq(Persistence(1), EditUI, SelectUI))
 
-  field("edition", PositiveIntQT, Seq(Persistence(1), EditUI))
+  field("edition", PositiveIntQT, Seq(Persistence(1), EditUI, DetailUI))
 
-  val genre = field(EnumerationValueQT[Genre](Genre), Seq(Persistence(1), EditUI, Default(Fantasy)))
+  val genre = field(EnumerationValueQT[Genre](Genre), Seq(Persistence(1), EditUI, DetailUI, Default(Fantasy)))
 
-  field(Publisher, Seq(Persistence(2), EditUI))
+  field(Publisher, Seq(Persistence(2), EditUI, DetailUI))
 
-  field("publishDate", DateWithoutTimeQT, Seq(Persistence(1), EditUI))
+  field("publishDate", DateWithoutTimeQT, Seq(Persistence(1), EditUI, DetailUI))
 }
