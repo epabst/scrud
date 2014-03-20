e54c75c60cc0d1974a68f5714d2582d12860c037
Eric Pabst <epabst@gmail.com>
2014-03-19 22:54:19 -0600

    wip with Action

---
+++ scrud-core/src/main/scala/com/github/scrud/action/Response.scala
@@ -0,0 +1,12 @@
+package com.github.scrud.action
+
+import com.netaporter.uri.Uri
+import com.github.scrud.copy.SourceType
+
+/**
+ * The response to an [[com.github.scrud.action.Action]].
+ * @author Eric Pabst (epabst@gmail.com)
+ *         Date: 3/19/14
+ *         Time: 7:30 PM
+ */
+case class Response(uri: Uri, sourceType: SourceType, source: AnyRef, headers: Map[String,String])
