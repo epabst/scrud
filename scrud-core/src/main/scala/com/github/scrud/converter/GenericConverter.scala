84f8e4b0ebfcce279475309551a508cb4742cfc2
Eric Pabst <epabst@gmail.com>
2014-05-08 23:36:32 -0600

    Fix warnings in GenericConverter.

--- scrud-core/src/main/scala/com/github/scrud/converter/GenericConverter.scala
+++ scrud-core/src/main/scala/com/github/scrud/converter/GenericConverter.scala
@@ -13,15 +13,15 @@ trait GenericConverter[-A,-B] {
 object GenericConverter {
   val stringToAnyVal: GenericConverter[String,AnyVal] = new SimpleGenericConverter[String,AnyVal] {
     def attemptConvertTo[T <: AnyVal](from: String)(implicit manifest: Manifest[T]): T = {
-      manifest.erasure match {
-        case x: Class[_] if (x == classOf[Int]) => from.toInt.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Long]) => from.toLong.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Short]) => from.toShort.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Byte]) => from.toByte.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Double]) => from.toDouble.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Float]) => from.toFloat.asInstanceOf[T]
-        case x: Class[_] if (x == classOf[Boolean]) => from.toBoolean.asInstanceOf[T]
-        case _ => throw new IllegalArgumentException("Unknown primitive type: " + classManifest.erasure +
+      manifest.runtimeClass match {
+        case x: Class[_] if x == classOf[Int] => from.toInt.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Long] => from.toLong.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Short] => from.toShort.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Byte] => from.toByte.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Double] => from.toDouble.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Float] => from.toFloat.asInstanceOf[T]
+        case x: Class[_] if x == classOf[Boolean] => from.toBoolean.asInstanceOf[T]
+        case _ => throw new IllegalArgumentException("Unknown primitive type: " + manifest.runtimeClass +
                 " with value " + from)
       }
     }
