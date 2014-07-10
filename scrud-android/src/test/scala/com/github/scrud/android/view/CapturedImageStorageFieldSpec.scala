b93723a9ea1be66459f0ab91e335e4d33a43c9de
Eric Pabst <epabst@gmail.com>
2014-07-10 00:37:19 -0600

    Fix failing test in CapturedImageStorageFieldSpec.

--- scrud-android/src/test/scala/com/github/scrud/android/view/CapturedImageStorageFieldSpec.scala
+++ scrud-android/src/test/scala/com/github/scrud/android/view/CapturedImageStorageFieldSpec.scala
@@ -1,9 +1,7 @@
 package com.github.scrud.android.view
 
 import org.junit.runner.RunWith
-import org.scalatest.matchers.MustMatchers
 import org.junit.Test
-import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
 import com.github.scrud.android.action.{ActivityResultDataField, ActivityResult}
 import android.net.Uri
@@ -11,13 +9,13 @@ import android.content.Intent
 import com.github.scrud.android._
 import com.github.scrud.copy.{ExtensibleAdaptableField, CopyContext}
 import com.github.scrud.UriPath
-import com.github.scrud.context.CommandContextForTesting
 import com.github.scrud.types.ImageQT
 import com.github.scrud.platform.representation.{EditUI, SelectUI}
-import scala.Some
 import com.netaporter
-import scala.Some
 import org.robolectric.annotation.Config
+import org.robolectric.Robolectric
+import com.github.scrud.android.action.AndroidOperation._
+import scala.Some
 
 /**
  * A behavior specification for [[com.github.scrud.android.view.CapturedImageStorageField]].
@@ -31,18 +29,22 @@ class CapturedImageStorageFieldSpec extends ScrudRobolectricSpec {
 
   @Test
   def capturedImageViewMustGetImageUriFromOperationResponse() {
+    val commandContext = Robolectric.buildActivity(classOf[CrudActivityForRobolectric]).
+      withIntent(new Intent(UpdateActionName)).get().commandContext
     val field = ActivityResultDataField
 
     val uri = Uri.parse("file://foo/bar.jpg")
     val intent = mock[Intent]
     stub(intent.getData).toReturn(uri)
 
-    val copyContext: CopyContext = new CopyContext(UriPath.EMPTY, new CommandContextForTesting(EntityTypeForTesting))
+    val copyContext: CopyContext = new CopyContext(UriPath.EMPTY, commandContext)
     field.findFieldValue(intent, copyContext) must be (Some(uri))
   }
 
   @Test
   def capturedImageViewMustGetImageUriFromOperationResponseEvenIfImageIsAlreadySet() {
+    val commandContext = Robolectric.buildActivity(classOf[CrudActivityForRobolectric]).
+      withIntent(new Intent(UpdateActionName)).get().commandContext
     val uri = Uri.parse("file://foo/bar.jpg")
     val intent = mock[Intent]
     stub(intent.getData).toReturn(uri)
@@ -50,9 +52,9 @@ class CapturedImageStorageFieldSpec extends ScrudRobolectricSpec {
     val entityType = new EntityTypeForTesting {
       val imageField = field("image", ImageQT, Seq(EditUI, SelectUI))
     }
-    val copyContext: CopyContext = new CopyContext(UriPath.EMPTY, new CommandContextForTesting(EntityTypeForTesting))
+    val copyContext: CopyContext = new CopyContext(UriPath.EMPTY, commandContext)
 
-    val viewRef = platformDriver.toViewRef("", entityType.imageField.fieldName)
+    val viewRef = platformDriver.toViewRef("edit_", entityType.imageField.fieldName)
     val adaptableField: ExtensibleAdaptableField[netaporter.uri.Uri] = entityType.imageField.toAdaptableField
     adaptableField.findSourceField(ActivityResult(viewRef)).get.findValue(intent, copyContext) must be (Some(uri))
   }
